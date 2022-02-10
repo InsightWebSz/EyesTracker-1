e3c653c41c4fb4a6879e82c70f8db11cf2800b2d
Yevhenii Rubanov <efsh49@gmail.com>
2022-02-10 22:35:50 +0200

    remove

--- Pods/Target Support Files/BBPortal/BBPortal-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BBPortal : NSObject
-@end
-@implementation PodsDummy_BBPortal
-@end
--- Pods/Target Support Files/DAFileMonitor/DAFileMonitor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DAFileMonitor : NSObject
-@end
-@implementation PodsDummy_DAFileMonitor
-@end
